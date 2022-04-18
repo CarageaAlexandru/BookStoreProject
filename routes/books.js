3f14c13bdad48822be1a29876c6a7708a7ec1d97
CarageaAlexandru <caragea.alexandru@yahoo.com>
2022-04-18 22:58:28 +0100

    Added form field for adding new books

--- routes/books.js
+++ routes/books.js
@@ -2,19 +2,29 @@ const express = require("express");
 const router = express.Router();
 // Update all the routes for book model
 const Book = require("../models/book");
+const Author = require("../models/author")
 
 //The route for all books
 
 router.get("/", async (req, res) => {
   // Search Authors
-  res.send("Receiving")
+  res.send("Receiving books")
 });
 
 // Route for new books only
-router.get("/new", (req, res) => {
+router.get("/new", async (req, res) => {
   // Import book on top
   // We can pass here the variables that will be send to ejs
-  res.send("Receiving new books")
+  try {
+    const authors = await Author.find({})
+    const book = new Book()
+    res.render('books/new', {
+      authors: authors,
+      book: book
+    })
+  } catch (error) {
+    res.redirect('/books')
+  }
 });
 
 // Create Book Route
